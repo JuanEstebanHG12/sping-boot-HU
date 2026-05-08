a190fa240d630f0c83834ab27aeb8f787ceb841e
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2026-05-08 07:00:59 -0500

    feat:models

--- src/main/java/com/example/riwiHU/model/Events.java
+++ src/main/java/com/example/riwiHU/model/Events.java
@@ -1,4 +1,17 @@
 package com.example.riwiHU.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Events {
+    int id;
+    String nombre;
+    LocalDateTime fecha;
+    String descripción;
 }
--- src/main/java/com/example/riwiHU/model/Venue.java
+++ src/main/java/com/example/riwiHU/model/Venue.java
@@ -1,4 +1,15 @@
 package com.example.riwiHU.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Venue {
+    int id;
+    String nombre;
+    String dirección;
+    int capacidad;
 }
