d9b3869eb8c45ace1bbe341660d4a17a437a32e9
JuanEstebanHG12 <juanestebanhg12@gmail.com>
2026-05-08 06:40:12 -0500

    first commit

---
+++ src/main/java/com/example/riwiHU/RiwiHuApplication.java
@@ -0,0 +1,13 @@
+package com.example.riwiHU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiwiHuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiwiHuApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/riwiHU/config/SwaggerConfig.java
@@ -0,0 +1,4 @@
+package com.example.riwiHU.config;
+
+public class SwaggerConfig {
+}
