854d9a73d60d6a1ec260f111219cb6d151bb1bb8
JuanEstebanHG12 <JuanEstebanHG12@gmail.com>
2026-05-15 11:51:00 -0500

    delete : props

--- src/main/java/com/example/riwiHU/model/Events.java
+++ src/main/java/com/example/riwiHU/model/Events.java
@@ -1,5 +1,6 @@
 package com.example.riwiHU.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,11 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "Events")
 public class Events {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
     String nombre;
     LocalDateTime fecha;
--- src/main/java/com/example/riwiHU/repository/GenericRepository.java
+++ src/main/java/com/example/riwiHU/repository/GenericRepository.java
@@ -1,10 +1,12 @@
 package com.example.riwiHU.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 import java.util.Optional;
 
-public interface GenericRepository <T, ID>{
-    T save(T entity);
-    List<T> findAll();
+public interface GenericRepository <T, ID> extends JpaRepository<T, ID> {
+    //T save(T entity);
+    //List<T> findAll();
     Optional<T> findById(ID id);
 }
